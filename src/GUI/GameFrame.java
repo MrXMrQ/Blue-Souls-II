bfb9b9900c449bb222a817781ee07ec0f8d94aa4
MrXMrQ <marvin@bl-automation.de>
2023-01-23 14:40:45 +0100

    .

--- src/GUI/GameFrame.java
+++ src/GUI/GameFrame.java
@@ -4,6 +4,7 @@ import Launcher.GameLauncher;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.Arrays;
 
 public class GameFrame {
     public static void gameFrame() {
@@ -18,17 +19,17 @@ public class GameFrame {
         LabelWithIcons cave1 = new LabelWithIcons();
         cave1.setPreferredSize(new Dimension(200,200));
         cave1.setBackground(Color.GRAY);
-        cave1.setText("Cave1");
+        cave1.setText(randomDungeons());
 
         LabelWithIcons cave2 = new LabelWithIcons();
         cave2.setPreferredSize(new Dimension(200,200));
         cave2.setBackground(Color.GRAY);
-        cave2.setText("Cave2");
+        cave2.setText(randomDungeons());
 
         LabelWithIcons cave3 = new LabelWithIcons();
         cave3.setPreferredSize(new Dimension(200,200));
         cave3.setBackground(Color.GRAY);
-        cave3.setText("Cave3");
+        cave3.setText(randomDungeons());
 
         panelCENTER.add(cave1);
         panelCENTER.add(cave2);
@@ -36,5 +37,12 @@ public class GameFrame {
 
         gameWindow.add(panelCENTER, BorderLayout.CENTER);
         System.out.println(GameLauncher.characterArray[0].getName() + GameLauncher.characterArray[0].getHealthpoints());
+    }
+
+    public static String randomDungeons() {
+        int dungeonsArrayLength = GameLauncher.dungeonsArray.length;
+        int randomDungeonNumber = (int)(Math.random()*dungeonsArrayLength);
+
+        return GameLauncher.dungeonsArray[randomDungeonNumber].getName();
     }
 }
--- src/GUI/MyFrame.java
+++ src/GUI/MyFrame.java
@@ -5,7 +5,7 @@ import javax.swing.*;
 public class MyFrame extends JFrame {
     public MyFrame() {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLocation(580,200);
+        setLocation(0,0);
         setSize(780, 480);
         setTitle(" Blue Souls II");
         setVisible(true);
