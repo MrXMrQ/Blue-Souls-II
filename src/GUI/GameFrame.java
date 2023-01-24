1818e5168fdcee4943a4645e49a63c42be056709
MrXMrQ <90854368+MrXMrQ@users.noreply.github.com>
2023-01-24 20:50:18 +0100

    .

--- src/GUI/GameFrame.java
+++ src/GUI/GameFrame.java
@@ -1,9 +1,13 @@
 package GUI;
 
+import Dungeons.Dungeons;
 import Launcher.GameLauncher;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class GameFrame {
     JButton button1;
@@ -18,19 +22,21 @@ public class GameFrame {
         MyFrame gameWindow = new MyFrame();
         gameWindow.setLayout(new BorderLayout());
 
+        randomDungeons();
+
         FlowLayout flowLayout = new FlowLayout();
         flowLayout.setHgap(25);
 
         JPanel panelNORTH = new JPanel(flowLayout);
 
         LabelWithIcons cave1 = new LabelWithIcons();
-        cave1.setText(randomDungeons());
+        cave1.setText(GameLauncher.dungeonsArray[0].getName());
 
         LabelWithIcons cave2 = new LabelWithIcons();
-        cave2.setText(randomDungeons());
+        cave2.setText(GameLauncher.dungeonsArray[1].getName());
 
         LabelWithIcons cave3 = new LabelWithIcons();
-        cave3.setText(randomDungeons());
+        cave3.setText(GameLauncher.dungeonsArray[2].getName());
 
         panelNORTH.add(cave1);
         panelNORTH.add(cave2);
@@ -41,13 +47,13 @@ public class GameFrame {
 
         JPanel panelCENTER = new JPanel(flowLayout);
 
-        button1 = new JButton();
+        button1 = new JButton(GameLauncher.dungeonsArray[0].getName());
         button1.setPreferredSize(new Dimension(256,64));
 
-        button2 = new JButton();
+        button2 = new JButton(GameLauncher.dungeonsArray[1].getName());
         button2.setPreferredSize(new Dimension(256,64));
 
-        button3 = new JButton();
+        button3 = new JButton(GameLauncher.dungeonsArray[2].getName());
         button3.setPreferredSize(new Dimension(256,64));
 
         panelCENTER.add(button1);
@@ -57,10 +63,10 @@ public class GameFrame {
         gameWindow.add(panelCENTER, BorderLayout.CENTER);
     }
 
-    public static String randomDungeons() {
-        int dungeonsArrayLength = GameLauncher.dungeonsArray.length;
-        int randomDungeonNumber = (int)(Math.random()*dungeonsArrayLength);
-        
-        return GameLauncher.dungeonsArray[randomDungeonNumber].getName();
+    public static void randomDungeons() {
+        List<Dungeons> list = Arrays.asList(GameLauncher.dungeonsArray);
+        Collections.shuffle(list);
+        list.toArray(GameLauncher.dungeonsArray);
+
     }
 }
